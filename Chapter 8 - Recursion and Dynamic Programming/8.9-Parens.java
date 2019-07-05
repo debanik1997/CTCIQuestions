3e31f8b8f5cb67f1fc2b32eb9459216206a4ebb2
Debanik Purkayastha <debanikpurkayastha@Debaniks-MacBook-Air.local>
2019-07-05 16:08:41 -0500

    8.9

--- Chapter 8 - Recursion and Dynamic Programming/8.9-Parens.java
+++ Chapter 8 - Recursion and Dynamic Programming/8.9-Parens.java
@@ -1,1 +1,20 @@
 // 8.9) Parens
+
+public void Parens(int n) {
+	Parens(n, new StringBuilder());
+}
+
+public void Parens(int n, StringBuilder sb, int open, int closed) {
+	if (sb.length() == n * 2 && open == closed) {
+		System.out.println(sb.toString());
+		return;
+	}
+
+	if (open < n) {
+		Parens(n, sb.append("("), ++open, closed);
+	}
+	if (closed < open) {
+		Parens(n, sb.append(")"), open, ++closed);
+	}
+
+}
